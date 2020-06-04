a2096f047dec97132a8ef31486a74cc920503887
Peter Zhao <peter.qs.zhao@gmail.com>
2020-06-04 12:48:51 -0400

    make db work

--- public/app.js
+++ public/app.js
@@ -1,6 +1,4 @@
 $(function () {
-  var backendHostUrl = "<your-backend-url>";
-
   var config = {
     apiKey: "AIzaSyDMElXSW444x1gyCNo2msWfnNBeTgEwbB0",
     authDomain: "peterzhao-1012.firebaseapp.com",
@@ -11,7 +9,7 @@ $(function () {
     appId: "1:129526295252:web:f3a89fd3e319d35978e941",
   };
 
-  var userIdToken = null;
+  var userId = null;
 
   function configureFirebaseLogin() {
     if (!firebase.apps.length) {
@@ -22,18 +20,12 @@ $(function () {
       if (user) {
         $("#logged-out").hide();
         var name = user.displayName;
-
         var welcomeName = name ? name : user.email;
-
-        user.getIdToken().then(function (idToken) {
-          userIdToken = idToken;
-
-          /* Now that the user is authenicated, fetch the notes. */
-          // fetchNotes();
-
-          $("#user").text(welcomeName);
-          $("#logged-in").show();
-        });
+        userId = user.uid;
+        $("#user").text(welcomeName);
+        $("#logged-in").show();
+        startToFetchNotes();
+        startToSaveNotes();
       } else {
         $("#logged-in").hide();
         $("#logged-out").show();
@@ -41,8 +33,6 @@ $(function () {
     });
   }
 
-  // [START configureFirebaseLoginWidget]
-  // Firebase log-in widget
   function configureFirebaseLoginWidget() {
     var uiConfig = {
       signInSuccessUrl: "/",
@@ -52,23 +42,37 @@ $(function () {
     var ui = new firebaseui.auth.AuthUI(firebase.auth());
     ui.start("#firebaseui-auth-container", uiConfig);
   }
-  function fetchNotes() {
-    $.ajax(backendHostUrl + "/notes", {
-      /* Set header for the XMLHttpRequest to get data from the web server
-      associated with userIdToken */
-      headers: {
-        Authorization: "Bearer " + userIdToken,
-      },
-    }).then(function (data) {
-      $("#notes-container").empty();
-      // Iterate over user data to display user's notes from database.
-      data.forEach(function (note) {
-        $("#notes-container").append($("<p>").text(note.message));
+
+  function startToFetchNotes() {
+    firebase
+      .database()
+      .ref("notes/" + userId)
+      .on("value", function (snapshot) {
+        var noteInDb = snapshot.val().note;
+        var noteField = $("#note-content");
+        var note = noteField.val();
+        if (note !== noteInDb) {
+          noteField.val(noteInDb);
+          console.log("*** updated local note from db");
+        }
       });
+  }
+
+  function startToSaveNotes() {
+    var noteField = $("#note-content");
+    noteField.on('keyup mouseup change', function () {
+      var note = noteField.val();
+      firebase
+        .database()
+        .ref("notes/" + userId)
+        .set({
+          note: note,
+        });
+      console.log("*** updated remote note from local");
     });
   }
-  var signOutBtn = $("#sign-out");
-  signOutBtn.click(function (event) {
+
+  $("#sign-out").click(function (event) {
     event.preventDefault();
 
     firebase
@@ -84,30 +88,8 @@ $(function () {
       );
   });
 
-  // Save a note to the backend
-  var saveNoteBtn = $("#add-note");
-  saveNoteBtn.click(function (event) {
-    event.preventDefault();
-
-    var noteField = $("#note-content");
-    var note = noteField.val();
-    noteField.val("");
-
-    /* Send note data to backend, storing in database with existing data
-    associated with userIdToken */
-    $.ajax(backendHostUrl + "/notes", {
-      headers: {
-        Authorization: "Bearer " + userIdToken,
-      },
-      method: "POST",
-      data: JSON.stringify({ message: note }),
-      contentType: "application/json",
-    }).then(function () {
-      // Refresh notebook display.
-      fetchNotes();
-    });
-  });
-
+  $("#logged-in").hide();
+  $("#logged-out").hide();
   configureFirebaseLogin();
   configureFirebaseLoginWidget();
 });
